916c090ceea0307651428c580c8b7ea1371467d7
FriesW <william.james.fries@gmail.com>
2018-10-13 22:24:54 -0500

    Server: login paths

--- server/app.py
+++ server/app.py
@@ -1,7 +1,24 @@
 from sanic import response
+import hashlib
+
+USERS = dict()
+USERS['tigerhacks2018Alpha@outlook.com'] = b'\xe2\xac-#:\x1f\xdfaPz\xdb\x8e\x11(\xf4\x86.\x9f\xf3Qg\xd2%\xc5vy\x96\x942\x97\xe5s'
+
+STATE = dict()
 
 def attach(sanic):
     
-    @sanic.route('/')
-    async def root(request):
-        return response.text('Hello There!')
+    sanic.static('/', 'web_assets/login.html')
+    sanic.static('/login.css', 'web_assets/login.css')
+    
+    @sanic.route('/login', methods=['POST'])
+    async def login(request):
+        print(request.json)
+        h = hashlib.sha256()
+        h.update(request.json['password'])
+        hp = h.digest()
+        user = request.json['user']
+        if user in USERS:
+            if USERS[user] == hp:
+                print('SUCCESS!')
+        
