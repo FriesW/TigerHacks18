c78ceeae8a4e7a33a8c3c3ca278dd0310f0c464f
FriesW <william.james.fries@gmail.com>
2018-10-13 23:43:58 -0500

    Auth mechanics

--- server/app.py
+++ server/app.py
@@ -1,10 +1,39 @@
 from sanic import response
 import hashlib
+from itsdangerous import Signer
+from os import urandom
+
+dangerous = Signer(urandom(32))
 
 USERS = dict()
 USERS['tigerhacks2018Alpha@outlook.com'] = b'\xe2\xac-#:\x1f\xdfaPz\xdb\x8e\x11(\xf4\x86.\x9f\xf3Qg\xd2%\xc5vy\x96\x942\x97\xe5s'
 
 STATE = dict()
+
+def authorized():
+    def decorator(f):
+        @wraps(f)
+        async def decorated_function(request, *args, **kwargs):
+            # run some method that checks the request
+            # for the client's authorization status
+            try:
+                c = request.cookies.get('auth')
+                print(c)
+                dangerous.unsign(c)
+                is_authorized = True
+            except:
+                is_authorized = False
+
+            if is_authorized:
+                # the user is authorized.
+                # run the handler method and return the response
+                response = await f(request, *args, **kwargs)
+                return response
+            else:
+                # the user is not authorized.
+                return response.text('not authorized', 403)
+        return decorated_function
+    return decorator
 
 def attach(sanic):
     
@@ -23,7 +52,12 @@ def attach(sanic):
         if fu in USERS:
             if USERS[fu] == hp:
                 res = response.redirect('/account', status=303)
-                res.cookies['test'] ='yoho'
+                res.cookies['auth'] = dangerous.sign(b'auth')
                 return res
         return response.redirect('/', status=303)
+    
+    @sanic.route('/account')
+    @authorized()
+    async def account(request):
+        return request.text('here we are',200)
         
